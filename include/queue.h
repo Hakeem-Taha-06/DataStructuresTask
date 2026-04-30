705a8d46d6537c22c67bac31a4fd81676b0bd747
Ziad Ashraf <aziad3083@gmail.com>
2026-05-01 01:58:23 +0300

    implementing circular queue array and testing it

--- include/queue.h
+++ include/queue.h
@@ -9,32 +9,37 @@ typedef struct {
 	node* tail;
 }queue_ll;
 
-typedef struct {
-	int* data;
-	int size, count;
-}queue_a;
-
 queue_ll* createLLQueue();
-
-queue_a* createArrayQueue(int size);
-
-void enqueue(queue_a* q, int value);
 
 void enqueue(queue_ll* q, int value);
 
-int dequeue(queue_a* q);
-
 int dequeue(queue_ll* q);
 
-bool isFull(queue_a* q);
-
-bool isEmpty(queue_a* q);
-
 bool isEmpty(queue_ll* q);
-
-int peek(queue_a* q);
 
 int peek(queue_ll* q);
 
 void display(queue_ll* q);
 
+// =================== Queue Array part ==================
+class QueueArr {
+private:
+	const int size;
+	int *arr;
+	int _front, rear;
+public:
+	QueueArr();
+	QueueArr(int size);
+
+	~QueueArr();
+
+	bool isEmpty();
+	bool isFull();
+
+	void enqueue(int value);
+	int dequeue();
+
+	void print();
+	int front();
+
+};
