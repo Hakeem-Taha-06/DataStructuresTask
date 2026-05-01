d7165c6c083a0852c925faf514c6ad5e1c3f66db
Hakeem-Taha-06 <hakeem.taha06@eng-st.cu.edu.eg>
2026-05-01 16:31:43 +0300

    add test cases for LL queue

--- src/main.cpp
+++ src/main.cpp
@@ -6,43 +6,19 @@
 #include "linked_list.h"
 using namespace std;
 int main(){
+	//Queue Linked List -------------------------------------------------------
 	/*
 	queue_ll* qll = createLLQueue();
 
-	for(int i = 1; i <= 10; ++i) enqueue(qll, 10*i);
-	
+	cout << "Before enqueue: ";
 	display(qll);
-	dequeue(qll);
-	dequeue(qll);
-	display(qll);
-
-	for (int i = 1; i <= 5; ++i) insertAtHead(ll, i);
-	display(ll);
-	for (int i = 10; i >= 6; --i) insertAtEnd(ll, i);
-	display(ll);
-
-	
-	stack_ll* sll = createLLStack();
-	cout << isEmpty(sll) << endl;
-	push(sll, 27);
-	push(sll, 28);
-	push(sll, 29);
-	push(sll, 30);
-	display(sll);
-	pop(sll);
-	pop(sll);
-	display(sll);
-	cout << peek(sll) << endl;
-	cout << isEmpty(sll) << endl;
-	*/
-
-	//Queue Linked List -------------------------------------------------------
-	queue_ll* qll = createLLQueue();
 
 	for (int i = 1; i <= 10; ++i) enqueue(qll, 10 * i);
+	cout << "After enqueue: ";
 	display(qll); //should be {100, 90, 80, 70, 60, 50, 40, 30, 20, 10}
 
 	cout << "dequeued " << dequeue(qll) << endl;
+	cout << "after 1 dequeue: ";
 	display(qll); //should be {90, 80, 70, 60, 50, 40, 30, 20, 10}
 
 	enqueue(qll, 100);
@@ -50,11 +26,15 @@ int main(){
 		cout << "dequeued " << dequeue(qll) << endl;
 	}
 
+	cout << "Attempting dequeue with queue empty: ";
 	dequeue(qll); //should say that queue is empty
 
 	deleteQueue(&qll); //should delete queue and set qll to NULL
+
+	cout << "Attmepting display after deleting queue: ";
 	display(qll); //should say that queue is NULL
-	
+	*/
+
 	//Queue Array--------------------------------------------------------------
 	/*
 	QueueArr q;
